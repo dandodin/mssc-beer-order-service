9ff97014b937a28105172c546394c3717b5a4dd1
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-22 09:03:58 -0700

    add call to pull beer details from beer service

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/model/BeerDto.java
@@ -0,0 +1,29 @@
+package guru.sfg.beer.order.service.services.beer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime updatedDate;
+}
---
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperDecorator.java
@@ -0,0 +1,43 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.services.beer.BeerService;
+import guru.sfg.beer.order.service.services.beer.model.BeerDto;
+import guru.sfg.beer.order.service.web.model.BeerOrderLineDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+
+    private BeerService beerService;
+    private BeerOrderLineMapper mapper;
+
+    @Autowired
+    public void setBeerService(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @Autowired
+    public void setMapper(BeerOrderLineMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line) {
+        BeerOrderLineDto dto = mapper.beerOrderLineToDto(line);
+        Optional<BeerDto> beerDto = beerService.getBeerByUpc(line.getUpc());
+        if (beerDto.isPresent()) {
+            dto.setBeerName(beerDto.get().getBeerName());
+            dto.setBeerStyle(beerDto.get().getBeerStyle());
+            dto.setPrice(beerDto.get().getPrice());
+            dto.setBeerId(beerDto.get().getId());
+        }
+        return dto;
+    }
+
+    @Override
+    public BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto) {
+        return mapper.dtoToBeerOrderLine(dto);
+    }
+}
